c481a2f5b139b2290962bd573eb221723d24a0db
SACHIN RANA <sachinrana2304@gmail.com>
2024-12-24 19:50:10 +0530

    test commit

--- src/app/components/ProductDetails.jsx
+++ src/app/components/ProductDetails.jsx
@@ -106,3 +106,5 @@ const ProductDetails = ({
 };
 
 export default ProductDetails;
+
+//hey
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 const geistSans = Geist({
